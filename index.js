8ca8a566d03a113febf37b9edfb6852524ed9b4d
saagarbhujel <saagarbhujel22@gmail.com>
2023-04-22 13:39:25 +0545

    heading correcter

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
-const celElement = document.getElementById("celsius")
+const celElement = document.getElementById("celsius");
 const fahrenElement = document.getElementById("fahrenheit");
-const kelElement = document.getElementById("kelvin")
-
+const kelElement = document.getElementById("kelvin");
 
 //clecius conversion
 
-celElement.addEventListener("change",(e)=>{
-console.log(e.target.value)
-let currentValue = +e.target.value
+celElement.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  let currentValue = +e.target.value;
 
-if(e.target.name === "celsius"){
-     fahrenElement.value = (currentValue*1.8+ 32)
-        kelElement.value = (currentValue + 273.32)   
-}
-
-})
+  if (e.target.name === "celsius") {
+    fahrenElement.value = currentValue * 1.8 + 32;
+    kelElement.value = currentValue + 273.32;
+  }
+});
 
 //fahrenhiet conversion
-fahrenElement.addEventListener("change",(e)=>{
-console.log(e.target.value)
-let currentValue = +e.target.value
- 
-if(e.target.name === "fahrenheit"){
-    celElement.value = ((currentValue - 32) / 1.8)
-    kelElement.value = ( (currentValue - 32) / 1.8 + 273.32)
-}
+fahrenElement.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  let currentValue = +e.target.value;
 
-})
+  if (e.target.name === "fahrenheit") {
+    celElement.value = (currentValue - 32) / 1.8;
+    kelElement.value = (currentValue - 32) / 1.8 + 273.32;
+  }
+});
 
 //kelvin conversion
 
-kelElement.addEventListener("change",(e)=>{
-console.log(e.target.value)
-let currentValue = +e.target.value
+kelElement.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  let currentValue = +e.target.value;
 
-if(e.target.name ==="kelvin"){
-    celElement.value = (currentValue - 273.32)
-    fahrenElement.value = ((currentValue - 273.32)*1.8 +32)
-}
-
-
-})
-
-
-
-
-
+  if (e.target.name === "kelvin") {
+    celElement.value = currentValue - 273.32;
+    fahrenElement.value = (currentValue - 273.32) * 1.8 + 32;
+  }
+});
 
 // function computeTemp(event){
 //     console.log(event.target.value)
